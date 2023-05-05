1881d0b31828705e72137fd637b1972010565b27
Zani231089 <231089@virtualwindow.co.za>
2023-05-05 13:36:27 +0200

    Final

---
+++ js/index.js
@@ -0,0 +1,17 @@
+let tim = 11;
+let jane = 20;
+let john = 10;
+
+tim = tim + 10;
+
+let teamTotal = tim + jane;
+let victory = john / 2;
+let total = teamTotal + victory;
+
+
+
+let answer = "Tim & Jane now have " + total + " marbles!";
+
+console.log(answer)
+
+
